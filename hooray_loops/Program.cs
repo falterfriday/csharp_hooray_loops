9308181c6cd7b92b861b9b69a350da0299737446
Patrick Todd <patrick.todd@reisystems.com>
2016-12-09 17:21:19 -0500

    using for loops

---
+++ hooray_loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hooray_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine("i equals: " + i);
+            }
+            string[] fruit = { "apples", "bananas", "Oranges" };
+
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                Console.WriteLine("the fruit of the day is: " + fruit[i]);
+            }
+        }
+    }
+}
