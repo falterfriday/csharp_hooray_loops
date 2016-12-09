b49a8c4e78e04e52289476777f2aaf6fd03d166a
Patrick Todd <patrick.todd@reisystems.com>
2016-12-09 17:22:48 -0500

    added while loop

--- hooray_loops/Program.cs
+++ hooray_loops/Program.cs
@@ -8,17 +8,28 @@ namespace hooray_loops
 {
     class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 0; i < 11; i++)
+        //    {
+        //        Console.WriteLine("i equals: " + i);
+        //    }
+        //    string[] fruit = { "Apples", "Bananas", "Oranges" };
+
+        //    for (int i = 0; i < fruit.Length; i++)
+        //    {
+        //        Console.WriteLine("the fruit of the day is: " + fruit[i]);
+        //    }
+        //}
         static void Main(string[] args)
         {
-            for (int i = 0; i < 11; i++)
-            {
-                Console.WriteLine("i equals: " + i);
-            }
-            string[] fruit = { "apples", "bananas", "Oranges" };
+            int counter = 1;
+            int max = 10;
 
-            for (int i = 0; i < fruit.Length; i++)
+            while (counter <= max)
             {
-                Console.WriteLine("the fruit of the day is: " + fruit[i]);
+                Console.WriteLine("Loop number: " + counter);
+                counter++;
             }
         }
     }
